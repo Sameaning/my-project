826d8eab31d860c0a2d67ba12542fcca55f2f94f
Sameaning <sam_mean@163.com>
2025-05-26 00:30:03 +0800

    Add subtraction function

--- src/main.py
+++ src/main.py
@@ -3,5 +3,10 @@ print('Hello World')
 def add(a, b):
     return a + b
 
+# 增加减法功能
+def subtract(a, b):
+    return a - b
+
 if __name__ == "__main__":
     print("2 + 3 =", add(2, 3))
+    print("5 - 3 =", subtract(5, 3))
