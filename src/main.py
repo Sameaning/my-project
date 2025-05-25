237234d722b297657e8e11e9697edd497bf10927
Sameaning <sam_mean@163.com>
2025-05-26 00:35:07 +0800

    Add addition function

--- src/main.py
+++ src/main.py
@@ -1,12 +1,7 @@
 print('Hello World')
-# 修改内容
+# 修改main.py内容
 def add(a, b):
     return a + b
 
-# 增加减法功能
-def subtract(a, b):
-    return a - b
-
 if __name__ == "__main__":
     print("2 + 3 =", add(2, 3))
-    print("5 - 3 =", subtract(5, 3))
